a90f97ec5b2ae445f602a32d9861ca6d1100c56f
albertsendrosjimenez <sendros@pm.me>
2024-06-15 01:39:06 +0200

    - modified style of the navbar when expanded for mobile - added custom 404 - setup robots.txt and sitemap - created hamburguerOnClick.js to have one script for all to be reused

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import {defineConfig} from 'astro/config';
 import preact from '@astrojs/preact';
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-        preact(), mdx()
+        preact(), mdx(), sitemap()
     ],
     prefetch: true,
+    site: "https://tiradas-ipsc.netlify.app"
 });
